54bbe909db6c22a6d593a648e0c00f067a303d53
ishimwejeanluc <ljeanluc394@gmail.com>
2025-09-12 19:33:54 +0200

    feat: adding dockerfiles  and dockercompose

---
+++ init.sql
@@ -0,0 +1,6 @@
+-- Create databases for each microservice
+CREATE DATABASE authservicevv;
+CREATE DATABASE merchantservicevv;
+CREATE DATABASE orderservicevv;
+CREATE DATABASE paymentservicevv;
+CREATE DATABASE riderservicevv;
